580bfceb7cfc3a07e558a747033b34027fa1943f
aktivtliv53-maker <aktivtliv53@gmail.com>
2026-04-07 01:09:27 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
+
 # ==============================================================================
 # نظام نِبْرَاس السيادي (Nibras Sovereign System) - الإصدار V71.3.1-FINAL
 # Protected Semantic Navigator - Omni Sovereign Layer
 # المستخدم المهيمن: محمّد
 # ==============================================================================
-
 import streamlit as st
 import pandas as pd
 import plotly.express as px
